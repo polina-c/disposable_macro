f7e0fda0468a4a83bc2764ef8f61dfce4fb7ba5c
Polina Cherkasova <polina.c@live.com>
2024-04-12 18:03:16 -0700

    -

---
+++ disposable_macro/lib/disposable_macro.dart
@@ -0,0 +1,2 @@
+export 'src/disposable_macro.dart';
+export 'package:flutter/foundation.dart';
--- disposable_macro/test/disposable_macro_test.dart
+++ disposable_macro/test/disposable_macro_test.dart
@@ -2,8 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:disposable_macro/src/disposable_macro.dart';
 
-void main() {
-  test('adds one to input values', () {
+@Disposable()
+class User {
+  final String name;
+  final int age;
 
-  });
+  User(this.name, this.age);
+}
+
+void main() {
+  test('adds one to input values', () {});
 }
